00d072628b3e449020b396bce2d04beaf20eb72b
Fabian155 <it231425@gmail.com>
2025-06-09 08:17:58 -0500

    completacion de la ruta en el segundo archivo apps.py

--- Aplicaciones/Plataformas/apps.py
+++ Aplicaciones/Plataformas/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PlataformasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Plataformas'
+    name = 'Aplicaciones.Plataformas'
