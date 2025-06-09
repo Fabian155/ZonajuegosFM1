5c08a9a9e382165933bad7e30da787b7fee42897
Fabian155 <it231425@gmail.com>
2025-06-09 08:16:58 -0500

    completacion de la ruta en apps.py

--- Aplicaciones/Videjouegos/apps.py
+++ Aplicaciones/Videjouegos/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class VidejouegosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Videjouegos'
+    name = 'Aplicaciones.Videjouegos'
