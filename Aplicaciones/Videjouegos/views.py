3772d33344007c8bb71fadb96891c7be2f6b0fa2
Fabian155 <it231425@gmail.com>
2025-06-09 08:31:50 -0500

    crecion de la funcion guardar

--- Aplicaciones/Videjouegos/views.py
+++ Aplicaciones/Videjouegos/views.py
@@ -15,16 +15,16 @@ def NuevoJuego(request):
     plataformas=Plataforma.objects.all()
     return render(request, "NuevoJuego.html", {'plataformas': plataformas})
 
-def GuardarC(request):
-    nombre=request.POST["nombre"]
-    descripcion=request.POST["descripcion"]
-    dificultad=request.POST["dificultad"]
-    duracion=request.POST["duracion"]
-    lugar=request.POST["lugar"]
-    instructor_id = request.POST['instructor']
-    instructor = Instructor.objects.get(id=instructor_id)
-    nuevoCurso=Curso.objects.create(nombre=nombre, descripcion=descripcion, dificultad=dificultad, duracion=duracion, lugar=lugar, instructor=instructor)
-    messages.success(request, "CURSO GUARDADO")
+def GuardarJ(request):
+    titulo=request.POST["titulo"]
+    genero=request.POST["genero"]
+    desarrollador=request.POST["desarollador"]
+    fecha_lanzamiento =request.POST["fecha_lanzamiento"]
+    clasificacion=request.POST["clasificacion"]
+    plataforma_id = request.POST['plataforma']
+    plataforma = Plataforma.objects.get(id=plataforma_id)
+    nuevaPLata=Videojuego.objects.create(titulo=titulo, genero=genero, desarrollador=desarrollador, fecha_lanzamiento=fecha_lanzamiento, clasificacion=clasificacion, plataforma=plataforma)
+    messages.success(request, " GUARDADO CORRECTA MENTE")
     return redirect('/')
 
 def EliminarCurso(request, id):
