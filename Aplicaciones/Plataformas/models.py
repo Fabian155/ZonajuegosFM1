f363fd78fa20037893102016e1c39a739305f459
Fabian155 <it231425@gmail.com>
2025-06-09 08:18:39 -0500

    creacion de las tablas en los models de las apps

--- Aplicaciones/Plataformas/models.py
+++ Aplicaciones/Plataformas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Plataforma(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.TextField()
+    fabricante = models.TextField()
+    anio_lanzamiento = models.IntegerField()
+    tipo = models.TextField()
+    region_disponible = models.TextField()
+
+    def __str__(self):
+        return self.nombre
--- Aplicaciones/Videjouegos/models.py
+++ Aplicaciones/Videjouegos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from Aplicaciones.Plataformas.models import Plataforma
 
 # Create your models here.
+
+class Videojuego(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.TextField()
+    genero = models.TextField()
+    desarrollador = models.TextField()
+    fecha_lanzamiento = models.DateField()
+    clasificacion = models.TextField()
+    plataforma = models.ForeignKey(Plataforma, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
